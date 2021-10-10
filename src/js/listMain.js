ab86d9c36cf3dfd62f3591aa0f1a8795c2bcaf3f
breeze <breezeye@foxmail.com>
2021-10-10 23:28:35 +0800

    模块化之前的备份，开始模块化编程

---
+++ src/js/listMain.js
@@ -0,0 +1,9 @@
+require.config({
+    baseUrl: "./",
+    paths: {
+        jq: "./libs/jquery",
+        gg: "./modules/getGoods",
+        ir: "./modules/indexRender",
+        sl: "./modules/setLocal"
+    }
+})
