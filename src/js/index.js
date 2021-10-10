8c427cee3543ffe752ab84aa2cce68ae4179e9ea
breeze <breezeye@foxemail.com>
2021-10-10 08:35:03 +0800

    修复了定时器的一个bug和改善表现

--- src/js/index.js
+++ src/js/index.js
@@ -603,27 +603,30 @@
 
     class TimeDown {
         constructor() {
-            this.end = document.querySelector(".timer");
+            this.end = document.querySelectorAll(".timer");
             this.day = document.querySelectorAll(".days");
             this.hours = document.querySelectorAll(".hours");
             this.minute = document.querySelectorAll(".minute");
             this.seconds = document.querySelectorAll(".seconds");
 
             setInterval(() => {
-                console.log(1);
                 this.getInterval();
             }, 1000);
         }
 
         getInterval() {
-            let interval = new Date(this.end.getAttribute("endtime")).getTime() - Date.now();
-            interval /= 1000;
-            for(let i = 0; i < this.day.length; i++) {
+            for (let i = 0; i < this.end.length; i++) {
+                let interval = new Date(this.end[i].getAttribute("endtime")).getTime() - Date.now();
+                interval /= 1000;
                 this.day[i].innerHTML = `${parseInt(interval / 60 / 60 / 24)}天`;
-                this.hours[i].innerHTML = parseInt(interval / 60 / 60 % 24);
-                this.minute[i].innerHTML = parseInt(interval / 60 % 60);
-                this.seconds[i].innerHTML = parseInt(interval % 60);
+                this.hours[i].innerHTML = this.add0(parseInt(interval / 60 / 60 % 24));
+                this.minute[i].innerHTML = this.add0(parseInt(interval / 60 % 60));
+                this.seconds[i].innerHTML = this.add0(parseInt(interval % 60));
             }
+        }
+
+        add0(value) {
+            return value < 10 ? `0${value}` : value
         }
     }
 
